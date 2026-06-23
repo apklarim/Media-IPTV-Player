efe1fbca00e54d3c9cb1acc57c797be947cb0c17
apklarim <apklarim@gmail.com>
2026-06-23 20:36:33 +0300

    Create Playlist.kt

---
+++ app/src/main/java/com/media/iptvplayer/model/Playlist.kt
@@ -0,0 +1,18 @@
+package com.media.iptvplayer.model
+
+data class Playlist(
+
+    val id: Long = System.currentTimeMillis(),
+
+    var name: String,
+
+    var type: String,
+
+    var url: String = "",
+
+    var username: String = "",
+
+    var password: String = "",
+
+    var server: String = ""
+)
