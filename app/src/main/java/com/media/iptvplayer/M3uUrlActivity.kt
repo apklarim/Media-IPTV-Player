d0af38e0961b8179a48f9e422f1e5aea020cd38c
apklarim <apklarim@gmail.com>
2026-06-23 21:21:41 +0300

    Update M3uUrlActivity.kt

--- app/src/main/java/com/media/iptvplayer/M3uUrlActivity.kt
+++ app/src/main/java/com/media/iptvplayer/M3uUrlActivity.kt
@@ -1,7 +1,11 @@
 package com.media.iptvplayer
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.media.iptvplayer.model.Playlist
 
 class M3uUrlActivity : AppCompatActivity() {
 
@@ -10,5 +14,48 @@ class M3uUrlActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_m3u_url)
+
+        val etName =
+            findViewById<EditText>(R.id.etPlaylistName)
+
+        val etUrl =
+            findViewById<EditText>(R.id.etPlaylistUrl)
+
+        findViewById<Button>(R.id.btnSaveM3u)
+            .setOnClickListener {
+
+                val name = etName.text.toString()
+
+                val url = etUrl.text.toString()
+
+                if (name.isEmpty() || url.isEmpty()) {
+
+                    Toast.makeText(
+                        this,
+                        "Lütfen tüm alanları doldurun",
+                        Toast.LENGTH_SHORT
+                    ).show()
+
+                    return@setOnClickListener
+                }
+
+                PlaylistManager.addPlaylist(
+                    this,
+
+                    Playlist(
+                        name = name,
+                        type = "M3U",
+                        url = url
+                    )
+                )
+
+                Toast.makeText(
+                    this,
+                    "Liste kaydedildi",
+                    Toast.LENGTH_SHORT
+                ).show()
+
+                finish()
+            }
     }
 }
