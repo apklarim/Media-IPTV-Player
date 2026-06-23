f66d8825dd7c6ff26e674653514e49e51f04ccd9
apklarim <apklarim@gmail.com>
2026-06-23 16:16:29 +0300

    Update MainActivity.kt

--- app/src/main/java/com/media/iptvplayer/MainActivity.kt
+++ app/src/main/java/com/media/iptvplayer/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.media.iptvplayer
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,50 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        // Canlı TV
+        findViewById<LinearLayout>(R.id.cardLive)
+            .setOnClickListener {
+
+                Toast.makeText(
+                    this,
+                    "Canlı TV yakında eklenecek",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        // Filmler
+        findViewById<LinearLayout>(R.id.cardMovies)
+            .setOnClickListener {
+
+                Toast.makeText(
+                    this,
+                    "Filmler yakında eklenecek",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        // Diziler
+        findViewById<LinearLayout>(R.id.cardSeries)
+            .setOnClickListener {
+
+                Toast.makeText(
+                    this,
+                    "Diziler yakında eklenecek",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+
+        // Ayarlar
+        findViewById<LinearLayout>(R.id.cardSettings)
+            .setOnClickListener {
+
+                startActivity(
+                    Intent(
+                        this,
+                        SettingsActivity::class.java
+                    )
+                )
+            }
     }
 }
