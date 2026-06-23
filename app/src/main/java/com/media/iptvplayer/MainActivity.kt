5e79c6923f48236394de49598b925f22e2ff464c
apklarim <apklarim@gmail.com>
2026-06-23 21:10:29 +0300

    Update MainActivity.kt

--- app/src/main/java/com/media/iptvplayer/MainActivity.kt
+++ app/src/main/java/com/media/iptvplayer/MainActivity.kt
@@ -3,50 +3,57 @@ package com.media.iptvplayer
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
 
         // Canlı TV
+
         findViewById<Button>(R.id.cardLive)
             .setOnClickListener {
 
-                Toast.makeText(
-                    this,
-                    "Canlı TV yakında eklenecek",
-                    Toast.LENGTH_SHORT
-                ).show()
+                startActivity(
+                    Intent(
+                        this,
+                        AddPlaylistActivity::class.java
+                    )
+                )
             }
 
         // Filmler
+
         findViewById<Button>(R.id.cardMovies)
             .setOnClickListener {
 
-                Toast.makeText(
-                    this,
-                    "Filmler yakında eklenecek",
-                    Toast.LENGTH_SHORT
-                ).show()
+                startActivity(
+                    Intent(
+                        this,
+                        AddPlaylistActivity::class.java
+                    )
+                )
             }
 
         // Diziler
+
         findViewById<Button>(R.id.cardSeries)
             .setOnClickListener {
 
-                Toast.makeText(
-                    this,
-                    "Diziler yakında eklenecek",
-                    Toast.LENGTH_SHORT
-                ).show()
+                startActivity(
+                    Intent(
+                        this,
+                        AddPlaylistActivity::class.java
+                    )
+                )
             }
 
         // Ayarlar
+
         findViewById<Button>(R.id.cardSettings)
             .setOnClickListener {
 
