e6c5e9f9d13c9512f72589e43e6575bc8a7356f8
apklarim <apklarim@gmail.com>
2026-06-23 20:37:18 +0300

    Create PlaylistManager.kt

---
+++ app/src/main/java/com/media/iptvplayer/PlaylistManager.kt
@@ -0,0 +1,59 @@
+package com.media.iptvplayer
+
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import com.media.iptvplayer.model.Playlist
+
+object PlaylistManager {
+
+    private const val PREFS = "media_iptv_player"
+    private const val KEY = "playlists"
+
+    fun getPlaylists(context: Context): MutableList<Playlist> {
+
+        val prefs = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
+
+        val json = prefs.getString(KEY, "[]")
+
+        val type = object : TypeToken<MutableList<Playlist>>() {}.type
+
+        return Gson().fromJson(json, type)
+    }
+
+    fun savePlaylists(
+        context: Context,
+        playlists: MutableList<Playlist>
+    ) {
+
+        val prefs = context.getSharedPreferences(PREFS, Context.MODE_PRIVATE)
+
+        prefs.edit()
+            .putString(KEY, Gson().toJson(playlists))
+            .apply()
+    }
+
+    fun addPlaylist(
+        context: Context,
+        playlist: Playlist
+    ) {
+
+        val list = getPlaylists(context)
+
+        list.add(playlist)
+
+        savePlaylists(context, list)
+    }
+
+    fun deletePlaylist(
+        context: Context,
+        id: Long
+    ) {
+
+        val list = getPlaylists(context)
+
+        list.removeAll { it.id == id }
+
+        savePlaylists(context, list)
+    }
+}
