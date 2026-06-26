c909ad9e872875152c8954751736915e028b1840
apklarim <apklarim@gmail.com>
2026-06-26 14:57:26 +0300

    Update SettingsActivity.kt

--- app/src/main/java/com/media/iptvplayer/SettingsActivity.kt
+++ app/src/main/java/com/media/iptvplayer/SettingsActivity.kt
@@ -1,13 +1,51 @@
 package com.media.iptvplayer
 
+import android.app.AlertDialog
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class SettingsActivity : AppCompatActivity() {
 
+    private lateinit var btnAbout: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
-        setContentView(R.layout.activity_settings)
+        setContentView(
+            R.layout.activity_settings
+        )
+
+        btnAbout =
+            findViewById(
+                R.id.btnAbout
+            )
+
+        btnAbout.setOnClickListener {
+
+            AlertDialog.Builder(this)
+                .setTitle(
+                    "Media IPTV Player"
+                )
+                .setMessage(
+                    """
+Sürüm : 1.1
+
+Media IPTV Player
+
+Telefon, Tablet,
+TV ve TV Box için
+gelişmiş IPTV oynatıcı.
+
+© 2026
+                    """.trimIndent()
+                )
+                .setPositiveButton(
+                    "Tamam",
+                    null
+                )
+                .show()
+        }
     }
 }
